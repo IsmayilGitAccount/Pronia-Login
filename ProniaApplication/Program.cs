6a8c3a423cc6fafe6bb53863bd3b367b59288bcd
IsmayilGitAccount <ismayilni-bp217@code.edu.az>
2024-12-04 11:12:42 -0800

    Initial commit

---
+++ ProniaApplication/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProniaApplication.DAL;
+using ProniaApplication.Models;
+
+namespace ProniaApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDBContext>(
+                opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"))
+
+                );
+            builder.Services.AddIdentity<AppUser, IdentityRole>(
+                opt =>
+                {
+                    opt.Password.RequiredLength = 8;
+                    opt.Password.RequireNonAlphanumeric = false;
+                    opt.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyz0123456789_@";
+                    opt.User.RequireUniqueEmail = true;
+
+                }
+                ).AddEntityFrameworkStores<AppDBContext>().AddDefaultTokenProviders();
+
+            var app = builder.Build();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseStaticFiles();
+
+            app.MapControllerRoute(
+               name: "admin",
+               pattern: "{area:exists}/{controller=home}/{action=index}/{id?}"
+               );
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=home}/{action=index}/{id?}"
+                );
+
+            app.Run();
+        }
+    }
+}
